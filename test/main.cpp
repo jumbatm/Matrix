4cb5d7f8b73ac6bbfcb0714d842a0a0ce9044070
Jonathan F <fungjys@gmail.com>
2018-08-02 11:30:05 +1000

    Add tests for zero-initialisation and element-wise multiplication

--- test/main.cpp
+++ test/main.cpp
@@ -4,11 +4,28 @@
 #include "Matrix.hpp"
 #include <vector>
 #include <utility>
+#include <random>
 
 using namespace mat;
 
+namespace
+{
 
-TEST_CASE("Construct Matrix")
+// For testing, bypassing the use of iterators.
+template <typename T, typename Function, size_t Rows, size_t Columns >
+void mat_for_each(Matrix<T, Rows, Columns>& mat, Function&& f)
+{
+    for (size_t i = 0; i < Rows; ++i)
+    for (size_t j = 0; j < Columns; ++j)
+    {
+        f(mat.at(i, j));
+    }
+}
+
+} // end namespace <anonymous>
+
+
+TEST_CASE("Construct Matrix with values")
 {
     Matrix<int, 3, 3> m = 
     {
@@ -23,6 +40,42 @@ TEST_CASE("Construct Matrix")
     for (size_t j = 0; j < 3; ++j)
     {
         REQUIRE(m.at(i, j) == expectedValue++);
+    }
+
+}
+
+TEST_CASE("Matrices are zero-initialised, no matter the size")
+{
+    // Perform this test 10 times.
+    for (size_t i = 0; i < 10; ++i)
+    {
+        auto *m = new Matrix<size_t, 10, 10>;
+        // We heap allocate so that we get the best chances of being allocated
+        // to different memory locations that hopefully have indeterminate
+        // values. This way, we can ensure zero-initialisation happens.
+
+        mat_for_each(*m, [](size_t& param)
+                {
+                REQUIRE(param == 0);
+                });
+        delete m;
+    }
+}
+
+TEST_CASE("Element-wise multiplication yields the expected results")
+{
+    constexpr int MATRIX_SIZE = 100;
+    std::random_device rd;
+
+    Matrix<int, MATRIX_SIZE, MATRIX_SIZE> m;
+    decltype(m) a;
+
+    auto t = m * a;
+    
+    for (size_t i = 0; i < MATRIX_SIZE; ++i)
+    for (size_t j = 0; j < MATRIX_SIZE; ++j)
+    {
+        REQUIRE(t.at(i, j) == m.at(i, j) * a.at(i, j));
     }
 
 }
