180d0d55854de9988ba21418d0ac81dbb88f3cde
Jonathan F <fungjys@gmail.com>
2018-08-03 20:02:48 +1000

    Add test to assert that dotproduct between differently-sized matrices cannot be achieved

--- test/main.cpp
+++ test/main.cpp
@@ -98,3 +98,14 @@ TEST_CASE("Matrix supports iterators. Order of traversal is row-major.") {
     }
   }
 }
+
+TEST_CASE("These should fail")
+{
+#if 0 // Dot product of two differently-sized matrices.
+    Matrix<int, 100, 100> m;
+    Matrix <int, 40, 20> a;
+
+    auto l = m * a;
+#endif
+
+}
