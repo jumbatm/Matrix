8220fd3635242a271d5b62ff8fce40fef251b2c8
Jonathan F <fungjys@gmail.com>
2018-08-04 13:47:14 +1000

    Separate iterators testing into own file

---
+++ test/iterators_test.cpp
@@ -0,0 +1,32 @@
+#include <array>
+#include "Matrix.hpp"
+#include "catch.hpp"
+#include "test_helpers.hpp"
+
+using namespace mat;
+
+TEST_CASE("Matrix supports iterators. Order of traversal is row-major.")
+{
+    constexpr int MATRIX_SIZE = 50;
+
+    std::array<int, MATRIX_SIZE> values;
+    initialise_random(values);
+
+    Matrix<int, MATRIX_SIZE / 2, MATRIX_SIZE> m;
+
+    // Put random values into our test matrix.
+    {
+        size_t index = 0;
+        mat_for_each(
+            m, [&values, &index](auto &param) { param = values[index++]; });
+    }
+
+    // Test that these values match.
+    {
+        size_t index = 0;
+        for (auto &elem : m)
+        {
+            REQUIRE(elem == values[index++]);
+        }
+    }
+}
--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,40 +1,9 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
-#include <utility>
-#include <vector>
-#include "Matrix.hpp"
-#include "test_helpers.hpp"
-
+#if 0
 using namespace mat;
 
-TEST_CASE("Matrix supports iterators. Order of traversal is row-major.")
-{
-    constexpr int MATRIX_SIZE = 50;
-
-    std::array<int, MATRIX_SIZE> values;
-    initialise_random(values);
-
-    Matrix<int, MATRIX_SIZE / 2, MATRIX_SIZE> m;
-
-    // Put random values into our test matrix.
-    {
-        size_t index = 0;
-        mat_for_each(
-            m, [&values, &index](auto &param) { param = values[index++]; });
-    }
-
-    // Test that these values match.
-    {
-        size_t index = 0;
-        for (auto &elem : m)
-        {
-            REQUIRE(elem == values[index++]);
-        }
-    }
-}
-
-#if 0
 TEST_CASE("These should fail")
 {
 #if 0  // Dot product of two differently-sized matrices.
@@ -45,4 +14,4 @@ TEST_CASE("These should fail")
 #endif
 
 }
-#endif  //
+#endif
