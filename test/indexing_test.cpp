42fdc979755446a6ce5dfeb109efb2e9c01eaa71
Jonathan F <fungjys@gmail.com>
2018-08-13 11:00:55 +1000

    Add tests for indexing

---
+++ test/indexing_test.cpp
@@ -0,0 +1,13 @@
+#include "catch.hpp"
+#include "Matrix.hpp"
+
+using namespace mat;
+
+TEST_CASE("Can modify matrix through .at")
+{
+    Matrix<int, 2, 2> m = { {1, 2}, {3, 4}};
+
+    m.at(0, 0) = 2;
+
+    REQUIRE(m.at(0, 0) == 2);
+}
