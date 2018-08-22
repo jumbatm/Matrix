e2caa531d529cc7b0de585d860b88410182d3e46
Jonathan F <fungjys@gmail.com>
2018-08-22 21:07:22 +1000

    Run autoformatter

--- test/operators_test.cpp
+++ test/operators_test.cpp
@@ -16,45 +16,44 @@ constexpr Matrix<int, 3, 3> testMatrix =
 // Dot product.
 TEST_CASE("Element-wise multiplication yields the expected results")
 {
-    constexpr int MATRIX_SIZE = 100;
+  constexpr int MATRIX_SIZE = 100;
 
-    Matrix<int, MATRIX_SIZE, MATRIX_SIZE> m;
-    initialise_random(m);
+  Matrix<int, MATRIX_SIZE, MATRIX_SIZE> m;
+  initialise_random(m);
 
-    decltype(m) a;
-    initialise_random(a);
+  decltype(m) a;
+  initialise_random(a);
 
-    Matrix<int, 100, 100> t =
-        m * a;  // Force conversion of expression to Matrix.
+  Matrix<int, 100, 100> t = m * a;  // Force conversion of expression to Matrix.
 
-    for (size_t i = 0; i < MATRIX_SIZE; ++i)
-        for (size_t j = 0; j < MATRIX_SIZE; ++j)
-        {
-            REQUIRE(t.at(i, j) == m.at(i, j) * a.at(i, j));
-        }
+  for (size_t i = 0; i < MATRIX_SIZE; ++i)
+    for (size_t j = 0; j < MATRIX_SIZE; ++j)
+    {
+      REQUIRE(t.at(i, j) == m.at(i, j) * a.at(i, j));
+    }
 }
 
-TEST_CASE("Multiplication expression template can do .at()") 
+TEST_CASE("Multiplication expression template can do .at()")
 {
-    constexpr int MATRIX_SIZE = 3;
+  constexpr int MATRIX_SIZE = 3;
 
-    auto exprTemplate = testMatrix * 1;
-    int expectedValue = 1;
+  auto exprTemplate = testMatrix * 1;
+  int expectedValue = 1;
 
-    for (size_t i = 0; i < MATRIX_SIZE; ++i)
-        for (size_t j = 0; j < MATRIX_SIZE; ++j)
-        {
-            REQUIRE(exprTemplate.at(i, j) == expectedValue++);
-        }
+  for (size_t i = 0; i < MATRIX_SIZE; ++i)
+    for (size_t j = 0; j < MATRIX_SIZE; ++j)
+    {
+      REQUIRE(exprTemplate.at(i, j) == expectedValue++);
+    }
 }
 
 TEST_CASE("Multiplication by scalar performs multiplication to each element.")
 {
-    Matrix<int, 3, 3> a = 3 * testMatrix;
+  Matrix<int, 3, 3> a = 3 * testMatrix;
 
-    int expectedValue = 1;
-    for (auto& elem : a)
-    {
-        REQUIRE(elem == (3 * expectedValue++));
-    }
+  int expectedValue = 1;
+  for (auto &elem : a)
+  {
+    REQUIRE(elem == (3 * expectedValue++));
+  }
 }
--- test/test_helpers.hpp
+++ test/test_helpers.hpp
@@ -17,28 +17,28 @@ void mat_for_each(mat::Matrix<T, Rows, Columns> &mat,
                   size_t &i = _empty_i,
                   size_t &j = _empty_j)
 {
-    for (i = 0; i < Rows; ++i)
-        for (j = 0; j < Columns; ++j)
-        {
-            f(mat.at(i, j));
-        }
+  for (i = 0; i < Rows; ++i)
+    for (j = 0; j < Columns; ++j)
+    {
+      f(mat.at(i, j));
+    }
 }
 
 template <typename Matrix>
 void initialise_random(Matrix &m)
 {
-    std::random_device rd;
-    mat_for_each(m, [&rd](auto &elem) { elem = rd(); });
+  std::random_device rd;
+  mat_for_each(m, [&rd](auto &elem) { elem = rd(); });
 }
 
 template <typename T, size_t size>
 void initialise_random(std::array<T, size> &toInitialise)
 {
-    std::random_device rd;
-    for (auto &elem : toInitialise)
-    {
-        elem = rd();
-    }
+  std::random_device rd;
+  for (auto &elem : toInitialise)
+  {
+    elem = rd();
+  }
 }
 
 }  // namespace
