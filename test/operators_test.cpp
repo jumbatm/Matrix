c5e8f8ae018658360fc43d7b388171efc384490a
Jonathan F <fungjys@gmail.com>
2018-08-26 23:05:18 +1000

    Add tests for elementwise addition and subtraction

--- test/operators_test.cpp
+++ test/operators_test.cpp
@@ -6,19 +6,20 @@ using namespace mat;
 
 namespace
 {
-constexpr Matrix<int, 3, 3> testMatrix =
-    // clang-format off
-{{ 1, 2, 3 }, 
- { 4, 5, 6 }, 
- { 7, 8, 9 }};
-// clang-format on
+constexpr Matrix<int, 3, 3> testMatrix = { { 1, 2, 3 },
+                                           { 4, 5, 6 },
+                                           { 7, 8, 9 } };
+
+constexpr Matrix<int, 3, 3> reversedTestMatrix = { { 9, 8, 7 },
+                                                   { 6, 5, 4 },
+                                                   { 3, 2, 1 } };
+
+constexpr int RANDOM_MATRIX_SIZE = 100;
 }  // namespace
 // Dot product.
 TEST_CASE("Element-wise multiplication yields the expected results")
 {
-  constexpr int MATRIX_SIZE = 100;
-
-  Matrix<int, MATRIX_SIZE, MATRIX_SIZE> m;
+  Matrix<int, RANDOM_MATRIX_SIZE, RANDOM_MATRIX_SIZE> m;
   initialise_random(m);
 
   decltype(m) a;
@@ -26,8 +27,8 @@ TEST_CASE("Element-wise multiplication yields the expected results")
 
   Matrix<int, 100, 100> t = m * a;  // Force conversion of expression to Matrix.
 
-  for (size_t i = 1; i <= MATRIX_SIZE; ++i)
-    for (size_t j = 1; j <= MATRIX_SIZE; ++j)
+  for (size_t i = 1; i <= RANDOM_MATRIX_SIZE; ++i)
+    for (size_t j = 1; j <= RANDOM_MATRIX_SIZE; ++j)
     {
       REQUIRE(t.at(i, j) == m.at(i, j) * a.at(i, j));
     }
@@ -35,13 +36,13 @@ TEST_CASE("Element-wise multiplication yields the expected results")
 
 TEST_CASE("Multiplication expression template can do .at()")
 {
-  constexpr int MATRIX_SIZE = 3;
+  constexpr int RANDOM_MATRIX_SIZE = 3;
 
   auto exprTemplate = testMatrix * 1;
   int expectedValue = 1;
 
-  for (size_t i = 1; i <= MATRIX_SIZE; ++i)
-    for (size_t j = 1; j <= MATRIX_SIZE; ++j)
+  for (size_t i = 1; i <= RANDOM_MATRIX_SIZE; ++i)
+    for (size_t j = 1; j <= RANDOM_MATRIX_SIZE; ++j)
     {
       REQUIRE(exprTemplate.at(i, j) == expectedValue++);
     }
@@ -56,4 +57,32 @@ TEST_CASE("Multiplication by scalar performs multiplication to each element.")
   {
     REQUIRE(elem == (3 * expectedValue++));
   }
+}
+
+TEST_CASE("Addition between two matrices performs elementwise sum.")
+{
+  Matrix<int, 3, 3> answer = testMatrix + reversedTestMatrix;
+  for (auto &elem : answer)
+  {
+    REQUIRE(elem == 10);
+  }
+}
+
+TEST_CASE("Subtraction between two matrices performs elementwise subtraction.")
+{
+  Matrix<int, RANDOM_MATRIX_SIZE, RANDOM_MATRIX_SIZE> m;
+  initialise_random(m);
+
+  Matrix<int, RANDOM_MATRIX_SIZE, RANDOM_MATRIX_SIZE> a;
+  initialise_random(a);
+
+  Matrix<int, RANDOM_MATRIX_SIZE, RANDOM_MATRIX_SIZE> t = m - a;
+
+  size_t i = 0, j = 0;
+
+  mat_for_each(
+      t,
+      [&a, &m, &i, &j](int val) { REQUIRE(val == m.at(i, j) - a.at(i, j)); },
+      i,
+      j);
 }
