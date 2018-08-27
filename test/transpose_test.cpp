708ac6075d82b63e3e7536b35817eb03deb61fce
Jonathan F <fungjys@gmail.com>
2018-08-27 20:45:13 +1000

    Run autoformatter

--- test/transpose_test.cpp
+++ test/transpose_test.cpp
@@ -1,17 +1,17 @@
 #include "Matrix.hpp"
-#include "test_helpers.hpp"
 #include "catch.hpp"
+#include "test_helpers.hpp"
 
 using namespace mat;
+
 TEST_CASE("Transpose expression template can take both lvalues and rvalues")
 {
   Matrix<int, 2, 2> test = { { 1, 2 }, { 3, 4 } };
   detail::_matrixTranspose<Matrix<int, 2, 2>> testTranspose(test);
 
-  for(size_t i = 1; i <= test.rows(); ++i)
-  for(size_t j = 1; i <= test.cols(); ++i)
-  {
-    REQUIRE(testTranspose.at(i, j) == test.at(j, i));
-  }
-
+  for (size_t i = 1; i <= test.rows(); ++i)
+    for (size_t j = 1; i <= test.cols(); ++i)
+    {
+      REQUIRE(testTranspose.at(i, j) == test.at(j, i));
+    }
 }
