87a568f2467740defef744bb278e646943a8b44a
Jonathan F <fungjys@gmail.com>
2018-08-28 22:25:16 +1000

    Add reference etc. + tests. Buff deduction guide so that it also wraps integrals.

---
+++ test/meta_test.cpp
@@ -0,0 +1,61 @@
+#include <type_traits>
+
+#include "Matrix.hpp"
+#include "catch.hpp"
+namespace
+{
+template <typename T>
+static void f()
+{
+  std::cout << __PRETTY_FUNCTION__ << "\n";
+}
+}  // namespace
+
+TEST_CASE("rereference properly re-applies an lvalue reference on same type")
+{
+  using ref  = int &;
+  using base = int;
+
+  using test = mat::detail::rereference_t<ref, base>;
+  REQUIRE(std::is_same_v<int &, test>);
+}
+
+TEST_CASE("rereference properly re-applies an rvalue reference on same type")
+{
+  using ref  = int &&;
+  using base = int;
+
+  using test = mat::detail::rereference_t<ref, base>;
+  REQUIRE(std::is_same_v<int &&, test>);
+}
+
+TEST_CASE("rereference properly re-applies a reference onto a different type")
+{
+  using ref  = int &&;
+  using base = float;
+
+  using test = mat::detail::rereference_t<ref, base>;
+  REQUIRE(std::is_same_v<float &&, test>);
+}
+
+TEST_CASE("wrap_if_integral_t correctly wraps integral types")
+{
+  REQUIRE(std::is_same_v<mat::detail::WrapIfIntegral_t<int>,
+                         mat::Matrix<int, 1, 1>>);
+}
+
+TEST_CASE("Wrap if integral wraps lvalue reference types")
+{
+  REQUIRE(std::is_same_v<mat::detail::WrapIfIntegral_t<int &>,
+                         mat::Matrix<int, 1, 1> &>);
+}
+TEST_CASE("Wrap if integral wraps rvalue reference types")
+{
+  REQUIRE(std::is_same_v<mat::detail::WrapIfIntegral_t<int &&>,
+                         mat::Matrix<int, 1, 1> &&>);
+}
+TEST_CASE("Wrap if integral wraps non-reference types")
+{
+  REQUIRE(std::is_same_v<mat::detail::WrapIfIntegral_t<int>,
+                         mat::Matrix<int, 1, 1>>);
+}
